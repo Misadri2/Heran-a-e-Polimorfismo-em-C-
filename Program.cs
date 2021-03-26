d4ce0e0ce22085d732fa6543206e9ebc0808f575
Your Name <mis.monterio@hotmail.com>
2021-03-26 14:43:49 -0300

    First Commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using ExHeranca.Entities;
+
+namespace ExHeranca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BusinessAccount account = new BusinessAccount(1155, "Mis Monteiro", 00.0, 500.0);
+
+            Console.WriteLine(account.Balance);
+        }
+    }
+}
