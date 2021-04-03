bcc74fe5aabbed175306cd9bd6787d3d4588ccf2
Your Name <mis.monterio@hotmail.com>
2021-04-03 17:38:44 -0300

    Ex done

--- Program.cs
+++ Program.cs
@@ -14,8 +14,36 @@ namespace ExPolimorfismo
             Console.Write("Enter the number of employees: ");
             int n = int.Parse(Console.ReadLine());
 
-            for(int i = 0; i < n ; i++){
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"Employee #{i} data:");
+                Console.Write("Outsourced (y/n)? ");
+                char ch = char.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                String name = Console.ReadLine();
+                Console.Write("Hours: ");
+                int hours = int.Parse(Console.ReadLine());
+                Console.Write("Value per hour: ");
+                double valuePerHour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                if (ch == 'y')
+                {
+                    Console.Write("Additional Charge: ");
+                    double additionalcharge = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    list.Add(new OutsourcedEmployee(name, hours, valuePerHour, additionalcharge));
 
+                }
+                else
+                {
+                    list.Add(new Employee(name, hours, valuePerHour));
+
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Payments: ");
+            foreach (Employee emp in list)
+            {
+                Console.WriteLine(emp.Name + " - $ " + emp.Payment().ToString("F2", CultureInfo.InvariantCulture));
             }
 
         }
